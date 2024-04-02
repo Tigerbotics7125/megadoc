30bdf248cf03e7e726b5c4aaa66adcb6144b0b2b
Jeffrey Morris <ffejcsirrom@gmail.com>
2024-04-01 23:29:56 -0500

    fix fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import '../styles.css'
+import '@fontsource/orbitron'
+import '@fontsource/inter'
 
 export default function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />;
